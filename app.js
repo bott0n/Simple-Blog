8970380de90e17d350a076e2d8afa20fd85fb5cd
bot_3310 <jacky2001114@gmail.com>
2018-03-11 16:08:58 +0800

    Update app.js

--- app.js
+++ app.js
@@ -10,8 +10,8 @@ var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 var mongo = require('mongodb');
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://admin:qq7895123@loginapp-shard-00-00-oa7mk.mongodb.net:27017,loginapp-shard-00-01-oa7mk.mongodb.net:27017,loginapp-shard-00-02-oa7mk.mongodb.net:27017/test?ssl=true&replicaSet=loginapp-shard-0&authSource=admin')
-//mongoose.connect('mongodb://localhost/loginapp')
+
+mongoose.connect('mongodb://localhost/loginapp')
 var db = mongoose.connection;
 
 
